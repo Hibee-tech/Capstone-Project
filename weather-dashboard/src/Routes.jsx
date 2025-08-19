0367ed5bb119df51d7106a7b90f74369a24012fe
hi bee <deanhorlar@gmail.com>
2025-08-19 17:54:32 +0200

    Set up routing with Routes.jsx, main.jsx, App.jsx page

---
+++ weather-dashboard/src/Routes.jsx
@@ -0,0 +1,24 @@
+
+import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
+import ScrollToTop from "components/ScrollToTop";
+import ErrorBoundary from "components/ErrorBoundary";
+import NotFound from "pages/NotFound";
+import GlobalWeatherOverviewDashboard from './pages/global-weather-overview-dashboard';
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <ErrorBoundary>
+      <ScrollToTop />
+      <RouterRoutes>
+        {/* Define your route here */}
+        <Route path="/" element={<GlobalWeatherOverviewDashboard />} />
+        <Route path="/global-weather-overview-dashboard" element={<GlobalWeatherOverviewDashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </RouterRoutes>
+      </ErrorBoundary>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
